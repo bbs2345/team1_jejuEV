43d37c8ebb5ab2211dfe6acd0814d3badf392d15
박범수 <cjswoqjatn1@naver.com>
2025-01-09 11:56:01 +0900

    보드 리스트 리드 구현

--- src/main/java/kr/co/mbc/repository/BoardRepository.java
+++ src/main/java/kr/co/mbc/repository/BoardRepository.java
@@ -1,9 +1,13 @@
 package kr.co.mbc.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import kr.co.mbc.entity.BoardEntity;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long>{
+
+	Optional<BoardEntity> findById(Long id);
 
 }
