58bcf55b521139f5a6630b3cd0ca7b7ee5bab23f
장봉준 <zxcqazqa2@naver.com>
2025-01-09 12:57:42 +0900

    회원 프로필이미지 기능 완료

--- src/main/java/kr/co/mbc/controller/MemberController.java
+++ src/main/java/kr/co/mbc/controller/MemberController.java
@@ -2,6 +2,7 @@ package kr.co.mbc.controller;
 
 import java.util.Map;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,22 +13,43 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import jakarta.validation.Valid;
 import kr.co.mbc.dto.MemberForm;
 import kr.co.mbc.dto.MemberResponse;
 import kr.co.mbc.entity.MemberEntity;
 import kr.co.mbc.service.MemberService;
+import kr.co.mbc.utils.UploadFileUtils;
 import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+	
+	private static final String SERVICENAME ="member";
+	
+	private final UploadFileUtils uploadFileUtils;
 
 	private final MemberService memberService;
 	
-	
+	// 회원 프로필이미지 불러오기
+	@GetMapping("/imgDisplay")
+	public ResponseEntity<byte[]> imgDisplay(String fullFileName) {
+		
+		/*
+		// 마지막 '/'의 인덱스를 찾고, 파일네임만 추출
+        String fileName = fullFileName.substring(fullFileName.lastIndexOf("/") + 1);
+        
+        // 마지막 '/'의 인덱스를 찾고, 경로만 추출
+        String filePath = fullFileName.substring(0, fullFileName.lastIndexOf("/"));
+		*/
+		ResponseEntity<byte[]> entity = uploadFileUtils.imgDisplay(fullFileName);
+        
+		return entity;
+	}
 	
 	// 아이디 중복체크
 	@PostMapping("/checkId")
@@ -49,6 +71,14 @@ public class MemberController {
 	@PostMapping("/delete")
 	public String username(String username) {
 		
+		MemberEntity memberEntity = memberService.findByUsername(username);
+		
+		String profileImage = memberEntity.getProfileImage();
+
+		if(profileImage != null) {
+			uploadFileUtils.deleteFile(profileImage);
+		}
+		
 		memberService.deleteByUsername(username);
 		
 		return "redirect:/";
@@ -56,10 +86,23 @@ public class MemberController {
 	
 	//회원정보 수정 처리
 	@PostMapping("/update")
-	public String update(MemberForm memberForm) {
+	public String update(MemberForm memberForm, MultipartHttpServletRequest mRequest) {
 		
 		MemberEntity memberEntity = memberService.findByUsername(memberForm.getUsername());
 		memberEntity.setName(memberForm.getName());
+		
+		MultipartFile multipartFile = mRequest.getFile("profileImage");
+		
+		if (!multipartFile.isEmpty()) {
+			String oldFileName = memberEntity.getProfileImage();
+			
+			if (!oldFileName.isEmpty()) {
+				uploadFileUtils.deleteFile(oldFileName);
+			}
+			
+			String newFileName = uploadFileUtils.uploadFile(multipartFile, SERVICENAME, memberForm.getUsername());
+			memberEntity.setProfileImage(newFileName);
+		}
 		
 		memberService.save(memberEntity);
 		
@@ -94,13 +137,20 @@ public class MemberController {
 
 	// 회원가입 처리
 	@PostMapping("/insert")
-	public String insert(@Valid @ModelAttribute("memberForm") MemberForm memberForm, BindingResult bindingResult) {
+	public String insert(@Valid @ModelAttribute("memberForm") MemberForm memberForm, BindingResult bindingResult, MultipartHttpServletRequest mRequest) {
 		
 		if(bindingResult.hasErrors()) {
 			return "member/insertForm";
 		}
 		
 		MemberEntity memberEntity = MemberEntity.toMemberEntity(memberForm);
+		
+		MultipartFile multipartFile = mRequest.getFile("profileImage");
+		
+		if (!multipartFile.isEmpty()) {
+			String fileName = uploadFileUtils.uploadFile(multipartFile, SERVICENAME, memberForm.getUsername());
+			memberEntity.setProfileImage(fileName);
+		}
 
 		memberService.save(memberEntity);
 
